7944f090e52e3ca908fa6756ce71247b1c56151e
metulburr <micahpage911@yahoo.com>
2020-01-25 18:57:35 -0500

    added basic menu system

--- data/states/menu.py
+++ data/states/menu.py
@@ -5,19 +5,39 @@ class Menu(tools.States):
     def __init__(self):
         tools.States.__init__(self)
         self.next = 'game'
-        self.info = label.TopLeftText((100,100), 'Press mouse button to start', None, prepare.SCREEN_RECT)
+        self.options = ['Play', 'Quit']
+        self.next_list = ['game', ]
+        self.pre_render_options()
+        self.from_bottom = 200
+        self.spacer = 75
     def cleanup(self):
         print('cleaning up Menu state stuff')
     def startup(self):
         print('starting Menu state stuff')
     def get_event(self, event):
-        if event.type == pg.KEYDOWN:
-            print('Menu State keydown')
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            self.done = True
+        if event.type == pg.QUIT:
+            self.quit = True
+        elif event.type == pg.KEYDOWN:
+            if event.key in [pg.K_UP, pg.K_w]:
+                self.change_selected_option(-1)
+            elif event.key in [pg.K_DOWN, pg.K_s]:
+                self.change_selected_option(1)
+                
+            elif event.key == pg.K_RETURN:
+                self.select_option(self.selected_index)
+        self.mouse_menu_click(event)
     def update(self, screen, dt, keys):
+
+        self.mouse_hover_sound()
+        self.change_selected_option()
         self.draw(screen)
     def draw(self, screen):
         screen.fill((255,0,0))
-        self.info.draw(prepare.SCREEN)
-        
+        for i,opt in enumerate(self.rendered["des"]):
+            opt[1].center = (prepare.SCREEN_RECT.centerx, self.from_bottom+i*self.spacer)
+            if i == self.selected_index:
+                rend_img,rend_rect = self.rendered["sel"][i]
+                rend_rect.center = opt[1].center
+                screen.blit(rend_img,rend_rect)
+            else:
+                screen.blit(opt[0],opt[1])
--- data/tools.py
+++ data/tools.py
@@ -1,12 +1,91 @@
 import os
 import pygame as pg
 
+class Font:
+    path = 'resources/fonts'
+    @staticmethod
+    def load(filename, size):
+        p = os.path.join(Font.path, filename)
+        return pg.font.Font(os.path.abspath(p), size)
+        
+class Sound:
+    def __init__(self, filename):
+        self.path = os.path.join('resources', 'sounds')
+        self.fullpath = os.path.join(self.path, filename)
+        pg.mixer.init(frequency=22050, size=-16, channels=2, buffer=128)
+        self.sound = pg.mixer.Sound(os.path.abspath(self.fullpath))
+
 class States(object):
     def __init__(self):
         self.done = False
         self.next = None
         self.quit = False
         self.previous = None
+        
+        
+        #self.menu_manager = MenuManager()
+        self.selected_index = 0
+        self.last_option = None
+        self.button_volume = .2
+        self.button_hover_volume = .1
+        self.button_sound = Sound('button.wav')
+        self.button_sound.sound.set_volume(self.button_volume)
+        self.button_hover = Sound('button_hover.wav')
+        self.button_hover.sound.set_volume(self.button_hover_volume)
+        
+    def mouse_hover_sound(self):
+        '''play sound when selected option changes'''
+        for i,opt in enumerate(self.rendered["des"]):
+            if opt[1].collidepoint(pg.mouse.get_pos()):
+                if self.last_option != opt:
+                    self.button_hover.sound.play()
+                    self.last_option = opt
+    def mouse_menu_click(self, event):
+        '''select menu option '''
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            for i,opt in enumerate(self.rendered["des"]):
+                if opt[1].collidepoint(pg.mouse.get_pos()):
+                    self.selected_index = i
+                    self.select_option(i)
+                    break
+    def pre_render_options(self):
+        '''render menu options based on selected or deselected'''
+        font_deselect = Font.load('impact.ttf', 50)
+        font_selected = Font.load('impact.ttf', 75)
+
+        rendered_msg = {"des":[],"sel":[]}
+        for option in self.options:
+            d_rend = font_deselect.render(option, 1, (255,255,255))
+            d_rect = d_rend.get_rect()
+            s_rend = font_selected.render(option, 1, (255,255,0))
+            s_rect = s_rend.get_rect()
+            rendered_msg["des"].append((d_rend,d_rect))
+            rendered_msg["sel"].append((s_rend,s_rect))
+        self.rendered = rendered_msg
+
+    def select_option(self, i):
+        '''select menu option via keys or mouse'''
+        if i == len(self.next_list):
+            self.quit = True
+        else:
+            self.button_sound.sound.play()
+            self.next = self.next_list[i]
+            self.done = True
+            self.selected_index = 0
+
+    def change_selected_option(self, op=0):
+        '''change highlighted menu option'''
+        for i,opt in enumerate(self.rendered["des"]):
+            if opt[1].collidepoint(pg.mouse.get_pos()):
+                self.selected_index = i
+        if op:
+            self.button_hover.sound.play()
+            self.selected_index += op
+            max_ind = len(self.rendered['des'])-1
+            if self.selected_index < 0:
+                self.selected_index = max_ind
+            elif self.selected_index > max_ind:
+                self.selected_index = 0
 
 def load_all_sfx(directory, accept=(".wav", ".mp3", ".ogg", ".mdi")):
     """
